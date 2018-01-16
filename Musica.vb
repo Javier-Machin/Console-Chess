0756211b1ea52fab18673ba042dbe37fc950bff0
Javier M <javier_machin_acosta@hotmail.com>
2018-01-16 00:25:31 +0000

    Delete Musica.vb

--- Musica.vb
+++
@@ -1,5 +0,0 @@
-﻿Module Musica
-    Sub musicafondo()
-        My.Computer.Audio.Play("E:\Users\Administrator\Documents\Visual Studio 2008\Projects\AjedrezF\AjedrezFinal\bin\50cent.wav", AudioPlayMode.BackgroundLoop)
-    End Sub
-End Module
